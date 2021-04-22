23d081164d0278dd118f899792271084c1c5f932
phellutone <72784756+phellutone@users.noreply.github.com>
2021-04-23 04:28:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,11 +14,19 @@ const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  ws.on("message", message => {
+    wss.clients.forEach(clients => {
+      ws.send(message);
+    });
+  });
   ws.on('close', () => console.log('Client disconnected'));
 });
 
+
+/*
 setInterval(() => {
   wss.clients.forEach((client) => {
     client.send(new Date().toTimeString());
   });
 }, 1000);
+*
