ad445a77c3403decffd9a7e72d022979058cecfe
phellutone <72784756+phellutone@users.noreply.github.com>
2021-04-23 00:59:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 'use strict';
 
+const express = require('express');
 const { Server } = require('ws');
 
-const PORT = process.env.PORT || 3906;
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
 
