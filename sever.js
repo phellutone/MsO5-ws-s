09db273f04fbf6ce55601fa91a8f604d6086ccb1
phellutone <72784756+phellutone@users.noreply.github.com>
2021-04-22 21:09:42 +0900

    Create sever.js

---
+++ sever.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3906;
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
