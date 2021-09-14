d6933ca2a9a28e648bf24c819c331fc5c1aaebc0
phellutone <phellutone@gmail.com>
2021-09-15 01:31:05 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,37 +8,35 @@ const INDEX = '/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
-  .on('request', req => console.log('url: '+req.url))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
 
-var clist =　[];
-var cc = 0;
+var clist = [];
+var hlist = [];
 
-wss.on('connection', ws => {
-  console.log('Client connected');
-  cc++;
+wss.on('connection', (ws, req) => {
+  console.log(`Client connected from: ${req.url}`);
+
+  if(req.url == '/c' && !clist.includes(ws)) clist.push(ws);
+  if(req.url == '/h' && !hlist.includes(ws)) hlist.push(ws);
 
   ws.on('message', message => {
-    if(!clist.includes(ws)) clist.push(ws);
-    
     wss.clients.forEach(client => {
-      if(!clist.includes(client)) client.send(cc+':'+message);
+      if(hlist.includes(client)) client.send(message);
     });
   });
+
   ws.on('close', () => {
     console.log('Client disconnected');
     clist = clist.filter(n => n != ws);
-    cc--;
+    hlist = hlist.filter(n => n != ws);
   });
 });
 
-
-
 setInterval(() => {
   wss.clients.forEach(client => {
+    if(clist.includes(client) || hlist.includes(client)) return;
     client.send(new Date().toTimeString()+"@"+Math.floor(Math.random() * 3)+"_"+Math.floor(Math.random() * 20));
   });
 }, 1000);
-
